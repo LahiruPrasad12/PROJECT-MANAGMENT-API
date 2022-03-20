790f64911d12dad71dfabedf68f0f21620bbd99c
LahiruPrasad12 <lahirupr471@gmail.com>
2022-03-20 14:45:55 +0530

    implemented protected router part

--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -3,6 +3,6 @@ const userController = require('../Controllers/userController');
 const authController = require('../Controllers/authController');
 const router = express.Router();
 
-router.get('/', authController.protect,userController.getAllUsers);
+router.get('/', authController.protect, authController.restrictTo('admin'), userController.getAllUsers);
 
 module.exports = router;
