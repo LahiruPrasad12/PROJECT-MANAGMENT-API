b2dc0c31ccd011d9e417a5ee1e17932424b13807
LahiruPrasad12 <lahirupr471@gmail.com>
2022-04-06 02:04:59 +0530

    implement email send part when user assign to group

--- Controllers/groupController.js
+++ Controllers/groupController.js
@@ -5,6 +5,7 @@ const FileUpload = require('../Utils/fileUpload');
 const multer = require('multer');
 const Group = require('../Models/groupModel');
 const User = require('../Models/userModel');
+const sendEmail = require('../Utils/email');
 
 
 
@@ -25,7 +26,7 @@ exports.getAllGroups = catchAsync(async (req, res, next) => {
 });
 
 
-exports.createGroup = catchAsync(async(req,res,next)=>{
+exports.createGroup = catchAsync(async (req, res, next) => {
     const newGroup = await Group.create(req.body)
     await User.findByIdAndUpdate(req.user.id, { groupID: newGroup.id })
     res.status(200).json({
@@ -37,13 +38,36 @@ exports.createGroup = catchAsync(async(req,res,next)=>{
 })
 
 //Assign student to group
-exports.assignGroup = catchAsync(async (req,res,next)=>{
-    const user = await User.findOne({ email: req.body.email , active:true });
-    if(!user) return next(new AppError('Please enter valid email',402))
+exports.assignGroup = catchAsync(async (req, res, next) => {
+    const user = await User.findOne({ email: req.body.email, active: true });
+    if (!user) return next(new AppError('Please enter valid email', 406))
+    if (user.groupID) return next(new AppError('This user already have an account', 404))
     user.groupID = req.user.groupID
     await user.save({ validateBeforeSave: false })
+
+
+    try {
+        await sendEmail({
+            email: req.body.email,
+            subject: 'Congratulations!!',
+            message :' You have been assign to group'
+        });
+
+        res.status(200).json({
+            status: 'success',
+            message: 'Your invitation has been sent'
+        });
+
+    } catch (err) {
+        return next(
+            new AppError('There was an error sending the email. Try again later!'),
+            500
+        );
+    }
+
+
     res.status(200).json({
         status: 'success',
-        
+
     });
 })
