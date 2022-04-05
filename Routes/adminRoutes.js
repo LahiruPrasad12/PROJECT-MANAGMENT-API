a902391970a338be6461b793ecf6c89b459635c3
LahiruPrasad12 <lahirupr471@gmail.com>
2022-04-05 22:30:38 +0530

    puted comment for better undestanding some code parts

--- Routes/adminRoutes.js
+++ Routes/adminRoutes.js
@@ -3,10 +3,12 @@ const adminController = require('../Controllers/adminController');
 const authController = require('../Controllers/authController');
 const router = express.Router();
 
+//This api-resource route for update and delete specific student
 router.route('/')
     .get(authController.protect, authController.restrictTo('admin'), adminController.getAllUsers)
     .post(authController.protect, authController.restrictTo('admin'), adminController.uploadDocument);
 
+//This api-resource route for update and delete specific student
 router.route('/:id')
     .delete(authController.protect,authController.restrictTo('admin'), adminController.deleteUser)
     .patch(authController.protect,authController.restrictTo('admin'), adminController.updateUser);
