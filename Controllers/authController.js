fb0b39d29b873a11e00733ec355a20f811a2e866
LahiruPrasad12 <lahirupr471@gmail.com>
2022-04-05 16:41:07 +0530

    implement user update part by admin

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -132,6 +132,27 @@ exports.deleteUser = catchAsync(async (req, res, next) => {
     data: null
   });
 });
+
+ //admin update user
+exports.updateUser = catchAsync(async (req, res, next) => {
+
+  // Filtered out unwanted fields names that are not allowed to be updated
+  const filteredBody = filterObj(req.body, 'name', 'email');
+
+  // Update user document
+  const updatedUser = await User.findByIdAndUpdate(req.params.id, filteredBody, {
+    new: true,
+    runValidators: true
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser
+    }
+  });
+});
+
 /**End of the auth controllers */
 
 
@@ -148,6 +169,15 @@ const signToken = id => {
     });
 };
 
+
+//filter and return column that needed to be updated
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach(el => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
 
 //crate new token and send it
 const createSendToken = (user, statusCode, res) => {
--- Routes/adminRoutes.js
+++ Routes/adminRoutes.js
@@ -4,6 +4,11 @@ const authController = require('../Controllers/authController');
 const router = express.Router();
 
 router.route('/')
-    .get(authController.protect, authController.restrictTo('admin'), userController.getAllUsers)
-router.delete('/:id',authController.protect,authController.restrictTo('admin'), authController.deleteUser);
+    .get(authController.protect, authController.restrictTo('admin'), userController.getAllUsers);
+
+router.route('/:id')
+    .delete(authController.protect,authController.restrictTo('admin'), authController.deleteUser)
+    .patch(authController.protect,authController.restrictTo('admin'), authController.updateUser);
+
+
 module.exports = router;
