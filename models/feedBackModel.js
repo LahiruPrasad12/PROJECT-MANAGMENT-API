3cbbfb5abf64afb4ee603f3036173f8737927881
LahiruPrasad12 <lahirupr471@gmail.com>
2022-05-24 04:14:00 +0530

    created model for feedback

--- models/feedBackModel.js
+++ models/feedBackModel.js
@@ -2,9 +2,25 @@ const mongoose = require('mongoose');
 
 const feedBackSchema = new mongoose.Schema({
 
-  topicID : String,
-  panel_member_id : String,
+  sender_id: {
+    type: String,
+    required: [true, 'Please Enter sender id'],
+  },
+  type: {
+    type: String,
+    enum: ['group', 'single','panel'],
+    required: [true, 'Please give message type'],
+  },
+  receiver_id: {
+    type: String,
+    required: [true, 'Please give receiver id'],
+  },
+  message: {
+    type: String,
+    required: [true, 'Please Enter message'],
+  },
+
 });
 
-const Group = mongoose.model('Groups', groupSchema);
-module.exports = Group;
+const Feedback = mongoose.model('Feedback', feedBackSchema);
+module.exports = Feedback;
