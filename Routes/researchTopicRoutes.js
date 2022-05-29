01e22ca7a83f9ce600bdabb8c4fcff3d0f9d1aba
Kavindu Lakshan <75882836+kavindu-lakshan@users.noreply.github.com>
2022-05-29 19:00:03 +0530

    implemented the rest api for get all research topic categories

--- Routes/researchTopicRoutes.js
+++ Routes/researchTopicRoutes.js
@@ -1,9 +1,11 @@
-const express = require('express');
-const topicController = require('../Controllers/researchTopicController');
-const authController = require('../Controllers/authController')
+const express = require("express");
+const topicController = require("../Controllers/researchTopicController");
+const authController = require("../Controllers/authController");
 const router = express.Router();
 
-router.route('/')
-    .post(topicController.createTopics)
+router
+  .route("/")
+  .post(topicController.createTopics)
+  .get(topicController.getTopics);
 
 module.exports = router;
