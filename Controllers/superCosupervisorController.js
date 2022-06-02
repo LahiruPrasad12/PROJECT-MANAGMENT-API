8adec1b8d3ce73b93ef6600cfccfba263bdc274d
Kavindu Lakshan <75882836+kavindu-lakshan@users.noreply.github.com>
2022-06-02 20:14:46 +0530

    Did some changes

--- Controllers/superCosupervisorController.js
+++ Controllers/superCosupervisorController.js
@@ -1,43 +1,42 @@
-const Group = require('../Models/groupModel');
-const catchAsync = require('../Utils/catchAsync');
+const Group = require("../Models/groupModel");
+const catchAsync = require("../Utils/catchAsync");
 const Filters = require("../Utils/filters");
 const Topic = require("../Models/topicModel");
 
 exports.getSupervisorRequest = catchAsync(async (req, res, next) => {
-  const Respond = await Topic.find({supervisorID:req.user._id})
+  const Respond = await Topic.find({ supervisorID: req.user._id });
+
   // SEND RESPONSE
   res.status(200).json({
-    status: 'success',
+    status: "success",
     results: Respond.length,
     data: {
-      Respond
-    }
+      Respond,
+    },
   });
 });
 
 exports.getCoSupervisorRequest = catchAsync(async (req, res, next) => {
-  const Respond = await Topic.find({supervisorID:req.user._id})
+  const Respond = await Topic.find({ supervisorID: req.user._id });
   // SEND RESPONSE
   res.status(200).json({
-    status: 'success',
+    status: "success",
     results: Respond.length,
     data: {
-      Respond
-    }
+      Respond,
+    },
   });
 });
-
 
 exports.acceptOrDeclineTopic = catchAsync(async (req, res, next) => {
   const { topic_id, status } = req.body;
-  const group = await Topic.findById(topic_id)
-  group.state = status
-   group.save()
+  const group = await Topic.findById(topic_id);
+  group.state = status;
+  group.save();
 
   // SEND RESPONSE
   res.status(200).json({
-    status: 'success',
+    status: "success",
     results: `${status} topic`,
   });
 });
-
