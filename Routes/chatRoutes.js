8fcf73e0d3d4eace80979fd9129d4b4917d1623d
Kavindu Lakshan <75882836+kavindu-lakshan@users.noreply.github.com>
2022-06-04 11:11:07 +0530

    Change routes and controllers

--- Routes/chatRoutes.js
+++ Routes/chatRoutes.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const chatController = require("../Controllers/chatControllers");
-const  authController  = require("../Controllers/authController");
+const authController = require("../Controllers/authController");
 
 const router = express.Router();
 
 router.route("/").post(authController.protect, chatController.accessChat);
 router.route("/").get(authController.protect, chatController.fetchChats);
-router.route("/chatGroup").post(authController.protect, chatController.createGroupChat);
+router
+  .route("/chatGroup")
+  .post(authController.protect, chatController.createGroupChat);
 router.route("/rename").put(authController.protect, chatController.renameGroup);
-router.route("/groupremove").put(authController.protect, chatController.removeFromGroup);
-router.route("/groupadd").put(authController.protect, chatController.addToGroup);
+router
+  .route("/groupremove")
+  .put(authController.protect, chatController.removeFromGroup);
+router
+  .route("/groupadd")
+  .put(authController.protect, chatController.addToGroup);
 
 module.exports = router;
