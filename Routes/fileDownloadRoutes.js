4be629219c3f4e6d69d6d5232d75e4e0e69faf41
LahiruPrasad12 <lahirupr471@gmail.com>
2022-06-04 13:11:21 +0530

    implemeneted download marching schema part

--- Routes/fileDownloadRoutes.js
+++ Routes/fileDownloadRoutes.js
@@ -5,6 +5,7 @@ const authController = require("../Controllers/authController");
 const router = express.Router();
 
 router.get('/topicDoc', authController.protect, download.downloadTopicDocument);
-router.get('/signing', authController.login);
+router.get('/marking-schema', authController.protect, download.downloadMarkingSchema);
+
 
 module.exports = router;
